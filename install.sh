3deb34c6839696dea6fb2eb5a7dbe8a7a4762cf8
liuzheng712 <liuzheng712@gmail.com>
2013-06-19 13:21:25 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 ln -sf `pwd`/.vimrc $HOME/
 ln -sf `pwd`/.vim $HOME/
-echo "source $PWD/.liuzheng_bashrc">>$HOME/.bashrc
-`source $HOME/.bashrc`
+
+if (cat $HOME/.bashrc|grep "source $PWD/.liuzheng_bashrc")
+then
+    echo "Already added"
+else
+    echo "source $PWD/.liuzheng_bashrc">>$HOME/.bashrc
+fi
+git submodule init
+git submodule update
+$HOME/.bashrc
