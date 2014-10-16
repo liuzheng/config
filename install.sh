4a9c315c1fcb1786b0787f75912a36eff4cc51cf
liuzheng712 <liuzheng712@gmail.com>
2014-10-16 13:27:38 +0800

    if git submodule update --depth 1 , it will bring some bug , because the depth does not aways at 1

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ then
 else
     echo "source $PWD/.liuzheng_bashrc">>$HOME/.bashrc
 fi
-git submodule update --init --depth 1
+git submodule update --init #--depth 1
 . ~/.bashrc
 sudo apt-get install ctags tmux cmake libpython-dev build-essential
 cd `pwd`/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive --depth 1
+git submodule update --init --recursive #--depth 1
 ./install.sh --clang-completer
