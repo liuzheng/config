7a401d433d8fe924235fbf3fea8ad331e03811ab
liuzheng <liuzheng712@gmail.com>
2021-09-24 23:15:52 +0800

    feat: update

--- install.sh
+++ install.sh
@@ -3,19 +3,16 @@ ln -s `pwd`/.vimrc $HOME/
 ln -s `pwd`/.vim $HOME/
 ln -s `pwd`/.inputrc $HOME/
 
-mkdir ~/.diary
-
 git submodule update --init --depth 1
 if (cat $HOME/.zshrc|grep "liuzheng_zshrc")
 then
     echo "Already added"
 else
-    echo "" >> $HOME/.bashrc
-    echo "source $PWD/.liuzheng_zshrc">>$HOME/.zshrc
+    echo "source $PWD/liuzheng_zshrc">$HOME/.zshrc
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 . ~/.zshrc
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install npm wget axel htop
-npm install -g commitizen
+npm install 
 
